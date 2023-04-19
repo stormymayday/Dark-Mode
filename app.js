e657590c0a95facc9883a6734faf3742def7bc09
stormymayday <stormymayday7@gmail.com>
2023-04-19 16:15:19 -0600

    Changing toggleBtn's textContent

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ toggleBtn.addEventListener('click', () => {
     // Targetting the HTML element and toggling the '.dark-theme' class
     document.documentElement.classList.toggle('dark-theme');
 
+    // Changing toggleBtn's textContent
+    if (document.documentElement.classList.contains('dark-theme')) {
+        toggleBtn.textContent = 'light';
+    } else {
+        toggleBtn.textContent = 'dark';
+    }
+
+
 });
